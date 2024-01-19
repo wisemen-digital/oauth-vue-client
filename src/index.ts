ce63807e605ba9ec19606b94c92f7a68036615a2
Wouter Laermans <wouter.laermans@appwise.be>
2024-01-19 20:58:49 +0100

    add OAuth2VueClient class for handling OAuth2 authentication and authorization

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,128 @@
-export const one = 1
-export const two = 2
+import type { OAuth2ClientTokensWithExpiration } from '@appwise/oauth2-client'
+import { OAuth2Client, TokenStore } from '@appwise/oauth2-client'
+import type { Axios, AxiosInstance, InternalAxiosRequestConfig } from 'axios'
+
+interface OAuth2VueClientOptions {
+  axios: Axios | AxiosInstance
+  clientId: string
+  clientSecret: string
+  tokenEndpoint: string
+  scopes?: string[]
+}
+
+export class OAuth2VueClient {
+  private oAuthFactory: OAuth2Client
+  private client: TokenStore | null = null
+
+  constructor(private readonly options: OAuth2VueClientOptions) {
+    const {
+      axios,
+      clientId,
+      clientSecret,
+      tokenEndpoint,
+    } = options
+
+    this.oAuthFactory = new OAuth2Client({
+      axios,
+      clientId,
+      clientSecret,
+      tokenEndpoint,
+    })
+
+    const tokens = this.loadTokensFromLocalStorage()
+
+    if (tokens !== null)
+      this.client = this.createClient(tokens)
+  }
+
+  private createClient(tokens: OAuth2ClientTokensWithExpiration): TokenStore {
+    const {
+      axios,
+      clientId,
+      clientSecret,
+      tokenEndpoint,
+    } = this.options
+
+    const client = new TokenStore(
+      {
+        axios,
+        clientId,
+        clientSecret,
+        tokenEndpoint,
+      },
+      tokens,
+    )
+
+    client.onRefreshToken((tokens) => {
+      this.saveTokensToLocalStorage(tokens)
+    })
+
+    return client
+  }
+
+  private saveTokensToLocalStorage(tokens: OAuth2ClientTokensWithExpiration | null): void {
+    if (tokens === null)
+      localStorage.removeItem('tokens')
+    else
+      localStorage.setItem('tokens', JSON.stringify(tokens))
+  }
+
+  private loadTokensFromLocalStorage(): OAuth2ClientTokensWithExpiration | null {
+    const tokens = localStorage.getItem('tokens')
+
+    if (tokens === null)
+      return null
+
+    const parsedTokens = JSON.parse(tokens) as OAuth2ClientTokensWithExpiration
+
+    return parsedTokens
+  }
+
+  public getClient(): TokenStore | null {
+    return this.client
+  }
+
+  private removeClient(): void {
+    this.client = null
+  }
+
+  public async login(username: string, password: string): Promise<void> {
+    const client = await this.oAuthFactory.login(username, password)
+
+    const tokens = client.getTokens()
+
+    this.saveTokensToLocalStorage(tokens)
+    this.client = this.createClient(tokens)
+  }
+
+  public logout(): void {
+    this.saveTokensToLocalStorage(null)
+    this.removeClient()
+  }
+
+  public isLoggedIn(): boolean {
+    const client = this.getClient()
+    return client?.getTokens() != null
+  }
+}
+
+export async function addAuthorizationHeader(
+  oAuthClient: OAuth2VueClient,
+  config: InternalAxiosRequestConfig<unknown>,
+): Promise<InternalAxiosRequestConfig<unknown>> {
+  const client = oAuthClient.getClient()
+
+  if (client === null)
+    return config
+
+  try {
+    const token = await client.getAccessToken()
+
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  catch {
+    console.warn('Failed to get access token, logging out')
+  }
+
+  return config
+}
